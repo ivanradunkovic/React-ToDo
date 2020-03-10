e8463fd49414761a05b64b90e4598188cbc35b74
Ivan <ivan1501@gmail.com>
2020-03-10 11:49:55 +0100

    Added new states for todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Create new app',
+      isCompleted: true,
+    },
+    {
+      content: 'Commit to git',
+      isCompleted: false,
+    },
+    {
+      content: 'npm start',
+      isCompleted: false,
+    }
+  ]);
+  
   return (
     <div className="app">
       <div className="header">
