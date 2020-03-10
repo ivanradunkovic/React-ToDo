63a2dda965ba3b1fbd0b12327a683795b283cbd5
Ivan <ivan1501@gmail.com>
2020-03-10 11:59:59 +0100

    Added event for todo. Now you can add new todo by pressing Enter key.All 3 todos from code are propper shown on page now.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,25 @@ function App() {
     }
   ]);
   
+  function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodoAtIndex(e, i);
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+  
+
   return (
     <div className="app">
       <div className="header">
@@ -25,10 +44,15 @@ function App() {
       </div>
       <form className="todo-list">
         <ul>
+          {todos.map((todo, i) => ( // it will show todos from function 
           <div className="todo">
             <div className="checkbox" />
-            <input type="text" value="First ToDo" />
+            <input type="text"
+            value={todo.content}
+            onKeyDown={e => handleKeyDown(e, i)}
+            />
           </div>
+           ))}
         </ul>
       </form>
     </div>
